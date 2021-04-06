2b25b01a3957a51daa164d5a3545c29877c52dc7
trestistt <80054683+trestistt@users.noreply.github.com>
2021-04-06 13:49:55 +0300

    landing v-1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.testimonials__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay:true,
+        // autoplaySpeed:6000,
+        fade: true,
+        arrows:false,
+        dots:true,
+    });
+});
